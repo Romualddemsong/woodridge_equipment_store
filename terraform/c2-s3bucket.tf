ffe2e709f909b829c84f61cab78ebb2506715662
Romualddemsong <romualddemsong@gmail.com>
2026-07-09 23:47:09 -0400

    terraform file being added

---
+++ terraform/c2-s3bucket.tf
@@ -0,0 +1,18 @@
+# because bucket name must be globally unique we need to generate a random name reason why we are using the random resource block
+resource "random_string" "random" {
+  length  = 16
+  Upper   = False
+  special = False
+}
+
+
+
+#provisioning an s3 bucket for storage
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-${random_string.random.result}"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
