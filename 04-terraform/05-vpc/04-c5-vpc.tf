72a28d5fdb9aeb09789fd8166e196d8c5a58ba6c
Romualddemsong <romualddemsong@gmail.com>
2026-07-15 00:37:58 -0400

    private and public subnet dynamically defined by replacing hardcoded cidr and az value to be dynamically configured

--- 04-terraform/05-vpc/04-c5-vpc.tf
+++ 04-terraform/05-vpc/04-c5-vpc.tf
@@ -6,9 +6,36 @@ resource "aws_vpc" "main"{
     enable_dns_hostnames = true
     tags =merge(var.tags, {Name = "${var.environment_name}-vpc"})
 }
+
 #resource 2 = internet gateways
+
+resource "aws_internet_gateway" "igw"{
+    vpc_id = aws_vpc.main.id
+    tags = merge(var.tags, {Name = "${var.environment_name}-igw"})
+lifecycle {
+    prevent_destroy = true
+}
+}
+
 #resource 3 = public subnet
+resource "aws_subnet" "public" {
+for_each = {for idx, az in local.azs : az => local.public_subnets[idx] }
+  vpc_id     = aws_vpc.main.id
+  cidr_block =each.value 
+  availability_zone = each.key
+  map_public_ip_on_launch = true
+tags = merge(var.tags, {Name = "${var.environment_name}-public-${each.key}"})
+
+}
 #resource 4 private subnet
+
+resource "aws_subnet" "private" {
+for_each = {for idx, az in local.azs : az => local.private_subnets[idx] }
+  vpc_id     = aws_vpc.main.id
+  cidr_block =each.value 
+  availability_zone = each.key
+  map_public_ip_on_launch = false
+tags = merge(var.tags, {Name = "${var.environment_name}-private-${each.key}"})
 #resource 5 =NAt gateway
 #resource 6 = elastic IP for NAT
 #resource 7 = public route tables
